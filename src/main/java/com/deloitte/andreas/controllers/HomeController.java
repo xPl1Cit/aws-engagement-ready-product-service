aa421c922cf8db4d312f988d7da38e2471e05798
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-14 21:25:32 +0200

    Fix

--- src/main/java/com/deloitte/andreas/controllers/HomeController.java
+++ src/main/java/com/deloitte/andreas/controllers/HomeController.java
@@ -4,23 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.deloitte.andreas.repository.ProductRepository;
-
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
-<<<<<<< HEAD
-    @Autowired
-    private ProductRepository repository;
-
-    @GetMapping()
-    public String showHome(String name) {
-=======
 
     @GetMapping()
     public String showHome(String name) {;
->>>>>>> a63111ea7b49ff05f2671cdbf526ffd510628c15
         return "Hey name, welcome to my project!";
     }
 
