a55783c11e1673b29842253ddfe40c533ac55079
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-15 01:41:20 +0200

    Fix

--- src/main/java/com/deloitte/andreas/entity/Cart.java
+++ src/main/java/com/deloitte/andreas/entity/Cart.java
@@ -4,15 +4,25 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
 @Table(name = "t_carts")
+@Data
 public class Cart {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long username;
     private Date expireDate;
+    @OneToMany
     private List<Product> products;
 
 }
--- src/main/java/com/deloitte/andreas/entity/Product.java
+++ src/main/java/com/deloitte/andreas/entity/Product.java
@@ -1,10 +1,21 @@
 package com.deloitte.andreas.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
 @Table(name = "t_products")
+@Data
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 
 }
